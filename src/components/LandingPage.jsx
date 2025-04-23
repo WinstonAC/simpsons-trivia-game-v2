9be654131620121c69163efa34e93abef83f3770
Billy_Cambell <Billy.campbell17@gmail.cOM>
2025-04-23 13:15:41 +0200

    fix: Update image directory naming - Rename Images directory to images for case consistency - Update import path in LandingPage component

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import donutImage from '../Images/doughnut.png';
+import donutImage from '../images/doughnut.png';
 import './LandingPage.css';
 
 const LandingPage = ({ onEnter }) => {
