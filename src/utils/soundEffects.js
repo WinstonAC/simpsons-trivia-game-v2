daad8838b2f22d8b26920c2fcec5398999d0c9be
Billy_Cambell <Billy.campbell17@gmail.cOM>
2025-04-23 13:14:29 +0200

    fix: Update sound file paths and structure - Move sound files to public/sounds directory - Update sound manager to use correct paths - Fix deployment error with missing sound files

--- src/utils/soundEffects.js
+++ src/utils/soundEffects.js
@@ -1,34 +1,27 @@
-import correctSound from '../assets/sounds/correct.mp3';
-import wrongSound from '../assets/sounds/wrong.mp3';
-import levelUpSound from '../assets/sounds/level-up.mp3';
-import gameOverSound from '../assets/sounds/game-over.mp3';
-
-class SoundManager {
-  constructor() {
-    this.sounds = {
-      correct: new Audio(correctSound),
-      wrong: new Audio(wrongSound),
-      levelUp: new Audio(levelUpSound),
-      gameOver: new Audio(gameOverSound)
-    };
-    this.isMuted = false;
-  }
+const soundManager = {
+  sounds: {
+    correct: new Audio('/sounds/woohoo.mp3'),
+    wrong: new Audio('/sounds/doh.mp3'),
+    levelUp: new Audio('/sounds/excellent.mp3'),
+    gameOver: new Audio('/sounds/ha-ha.mp3')
+  },
+  isMuted: false,
 
   play(soundName) {
     if (!this.isMuted && this.sounds[soundName]) {
       this.sounds[soundName].currentTime = 0;
       this.sounds[soundName].play();
     }
-  }
+  },
 
   toggleMute() {
     this.isMuted = !this.isMuted;
     return this.isMuted;
-  }
+  },
 
   setMute(isMuted) {
     this.isMuted = isMuted;
   }
-}
+};
 
-export const soundManager = new SoundManager(); 
+export { soundManager }; 
